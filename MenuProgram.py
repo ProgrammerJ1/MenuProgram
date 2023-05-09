7fafff5892b4d6bf23b61375a9b38fd05d7933ad
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-05-09 16:22:01 -0400

    Added Rotation Option

--- MenuProgram.py
+++ MenuProgram.py
@@ -20,7 +20,7 @@ import time
 import types
 import random
 class Grade:
-    def __init__(self) -> None:
+    def __init__(self,Response: function) -> None:
         self.Score=0
         self.Points=0
     def correct(self):
@@ -33,7 +33,7 @@ def Printing(String:str,delay:float=0.0625,enddelay:float=1):
         print(i,end="",flush=True)
         time.sleep(delay)
     time.sleep(enddelay)
-PointsDictionary:dict={"Math Question":Grade(),"List Question":Grade()}
+PointsDictionary={"Math Question":Grade(),"Rotation Question":Grade()}
 def MathQuestion():
     global PointsDictionary
     Choice=0
@@ -46,13 +46,44 @@ def MathQuestion():
         Printing(Operations[Choice].format(a,b)+"=")
     c=float(input())
     if c!=eval(Operations[Choice].format(a,b)):
-        Printing("Unfortunately, that answer is not correct.\n")
+        Printing("Unfortunately, that answer is not correct.\n",enddelay=2)
+        Printing("The correct awnser is {}",eval(Operations[Choice].format(a,b)))
         PointsDictionary["Math Question"].incorrect()
     else:
         Printing("Correct\n")
         PointsDictionary["Math Question"].correct()
-def MathDataSetQuestion():
-    x=None
+def RotationOption():
+    global PointsDictionary
+    orgnums=[]
+    Size=random.randint(5,10)
+    for i in range(Size):
+        orgnums.append(random.randint(1,100))
+    nums=orgnums
+    Direction=random.randint(0,1)
+    NumofRots=random.randint(1,Size-1)
+    Printing("My list of numbers currently equals {}".format(nums))
+    Printing("Now I decide to Rotate my list {} times {}".format(NumofRots,["left","right"][Direction]),enddelay=2)
+    RotResults=[]
+    if Direction:
+        for i in range(NumofRots):
+            nums[-1:] + nums[:-1]
+            RotResults.append(nums[0])
+    else:
+        for i in range(NumofRots):
+            nums[1:] + nums[:1]
+            RotResults.append(nums[0])
+    Printing("I just finshed rotating the list.",enddelay=2)
+    Printing("Now, what is the first element of the new list: ")
+    x=int(input())
+    if x==nums[0]:
+        Printing("You are correct",enddelay=2)
+        PointsDictionary["Rotation Question"].correct()
+    else:
+        Printing("That is incorrect")
+        for i in range(NumofRots):
+            Printing("Rotation {} made {} the first element".format(i+1,RotResults[i]),enddelay=2)
+        PointsDictionary["Rotation Question"].incorrect()
+
 Printing("Welcome to the Tool\n",enddelay=2)
 '''
 while True:
